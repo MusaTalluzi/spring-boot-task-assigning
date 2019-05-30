a44068706ca8b56d14720c7a912efa4cb0e4b4d1
Musa Talluzi <mtalluzi@redhat.com>
2019-05-30 14:11:19 -0400

    Initial commit

---
+++ src/main/java/org/optaplanner/springboottaskassigning/SpringBootTaskAssigningApplication.java
@@ -0,0 +1,13 @@
+package org.optaplanner.springboottaskassigning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskAssigningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskAssigningApplication.class, args);
+	}
+
+}
