9112e168ca79dce0558ec27cde3c3017ddbf9221
Musa Talluzi <mtalluzi@redhat.com>
2019-06-27 10:09:42 -0400

    Change API to accept problemId as Object instead of Comparable<?> tenantId and fast fail if already exists

--- src/main/java/org/optaplanner/springboottaskassigning/solver/DefaultSolverManager.java
+++ src/main/java/org/optaplanner/springboottaskassigning/solver/DefaultSolverManager.java
@@ -38,66 +38,66 @@ public class DefaultSolverManager<Solution_> implements SolverManager<Solution_>
 
     private ExecutorService executorService;
     private SolverFactory<Solution_> solverFactory;
-    private Map<Comparable<?>, SolverTask<Solution_>> tenantIdToSolverTaskMap;
+    private Map<Object, SolverTask<Solution_>> problemIdToSolverTaskMap;
 
     // TODO SolverManager should be SOLVER_CONFIG agnostic, it should take the solver configuration as a constructor argument
     // TODO i.e. InputStream/File ...
     public DefaultSolverManager() {
         solverFactory = SolverFactory.createFromXmlResource(SOLVER_CONFIG, DefaultSolverManager.class.getClassLoader());
-        tenantIdToSolverTaskMap = new HashMap<>();
+        problemIdToSolverTaskMap = new HashMap<>();
         int numAvailableProcessors = Runtime.getRuntime().availableProcessors();
         logger.info("Number of available processors: {}.", numAvailableProcessors);
         executorService = Executors.newFixedThreadPool(numAvailableProcessors - 2);
     }
 
     @Override
-    public void solve(Comparable<?> tenantId, Solution_ planningProblem,
+    public void solve(Object problemId, Solution_ planningProblem,
                       Consumer<Solution_> onBestSolutionChangedEvent, Consumer<Solution_> onSolvingEnded) {
         synchronized (this) {
-            if (tenantIdToSolverTaskMap.containsKey(tenantId)) {
-                throw new IllegalArgumentException("Tenant id (" + tenantId + ") already exists.");
+            if (problemIdToSolverTaskMap.containsKey(problemId)) {
+                throw new IllegalArgumentException("Problem (" + problemId + ") already exists.");
             }
-            SolverTask<Solution_> newSolverTask = new SolverTask<>(tenantId, solverFactory.buildSolver(), planningProblem,
+            SolverTask<Solution_> newSolverTask = new SolverTask<>(problemId, solverFactory.buildSolver(), planningProblem,
                     onSolvingEnded);
             // TODO implement throttling
             if (onBestSolutionChangedEvent != null) {
                 newSolverTask.addEventListener(bestSolutionChangedEvent -> onBestSolutionChangedEvent.accept(bestSolutionChangedEvent.getNewBestSolution()));
             }
             executorService.submit(newSolverTask);
-            tenantIdToSolverTaskMap.put(tenantId, newSolverTask);
-            logger.info("A new solver task was created with tenantId ({}).", tenantId);
+            problemIdToSolverTaskMap.put(problemId, newSolverTask);
+            logger.info("A new solver task was created with problemId ({}).", problemId);
         }
     }
 
     @Override
-    public Optional<Solution_> getBestSolution(Comparable<?> tenantId) {
-        logger.debug("Getting best solution of tenantId ({}).", tenantId);
-        SolverTask<Solution_> solverTask = tenantIdToSolverTaskMap.get(tenantId);
+    public Optional<Solution_> getBestSolution(Object problemId) {
+        logger.debug("Getting best solution of problemId ({}).", problemId);
+        SolverTask<Solution_> solverTask = problemIdToSolverTaskMap.get(problemId);
         return solverTask == null ? Optional.empty() : Optional.ofNullable(solverTask.getBestSolution());
     }
 
     @Override
-    public Optional<Score> getBestScore(Comparable<?> tenantId) {
-        logger.debug("Getting best score of tenantId ({}).", tenantId);
-        SolverTask<Solution_> solverTask = tenantIdToSolverTaskMap.get(tenantId);
+    public Optional<Score> getBestScore(Object problemId) {
+        logger.debug("Getting best score of problemId ({}).", problemId);
+        SolverTask<Solution_> solverTask = problemIdToSolverTaskMap.get(problemId);
         return solverTask == null ? Optional.empty() : Optional.ofNullable(solverTask.getBestScore());
     }
 
     @Override
-    public Optional<SolverStatus> getSolverStatus(Comparable<?> tenantId) {
-        logger.debug("Getting solver status of tenantId ({}).", tenantId);
-        SolverTask<Solution_> solverTask = tenantIdToSolverTaskMap.get(tenantId);
+    public Optional<SolverStatus> getSolverStatus(Object problemId) {
+        logger.debug("Getting solver status of problemId ({}).", problemId);
+        SolverTask<Solution_> solverTask = problemIdToSolverTaskMap.get(problemId);
         return solverTask == null ? Optional.empty() : Optional.ofNullable(solverTask.getSolverStatus());
     }
 
     @Override
-    public void addEventListener(Comparable<?> tenantId, SolverEventListener<Solution_> eventListener) {
-        logger.debug("Adding an event listener for tenantId ({}).", tenantId);
-        SolverTask<Solution_> solverTask = tenantIdToSolverTaskMap.get(tenantId);
+    public void addEventListener(Object problemId, SolverEventListener<Solution_> eventListener) {
+        logger.debug("Adding an event listener for problemId ({}).", problemId);
+        SolverTask<Solution_> solverTask = problemIdToSolverTaskMap.get(problemId);
         if (solverTask != null) {
             solverTask.addEventListener(eventListener);
         } else {
-            logger.error("Tenant ({}) does not have a SolverTask submitted.", tenantId);
+            logger.error("Problem ({}) does not have a SolverTask submitted.", problemId);
         }
     }
 
